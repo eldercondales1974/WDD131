52964440c02b78b0e09adc9e6a4dc39c5275d58a
eldercondales1974 <eldercondales@proton.me>
2026-05-07 15:14:28 -0400

    Last Commit today 2

--- mission/mission.js
+++ mission/mission.js
@@ -6,7 +6,7 @@ function changeTheme() {
     const selectedTheme = themeSelector.value;
     if (selectedTheme === 'dark') {
         body.classList.add('dark');
-        logoImg.src = 'images/byui-logo_white.png';
+        logoImg.src = 'images/byui-logo-white.png';
     }
     else{
         body.classList.remove('dark');
@@ -17,39 +17,7 @@ function changeTheme() {
 themeSelector.addEventListener('change', changeTheme);
 
 
-let selectElem = document.querySelector('select');
-let logo = document.querySelector('#logo');
-let body = document.querySelector('body');
 
-selectElem.addEventListener('change', changeTheme);
-
-//function changeTheme() {
-    //let current = selectElem.value;
-    //console.log(current);
-    //if (current === 'dark') {
-        // code for changes to colors and logo
-        //document.body.style.backgroundColor = 'black';
-        //document.body.style.color = '#ffffff';
-        //logo.setAttribute('src', 'images/byui-logo_white.png');
-
-    //} else {
-        // code for changes to colors and logo
-        //document.body.style.backgroundColor = '#ffffff';
-        //document.body.style.color = '#000000';
-        //logo.setAttribute('src', 'images/byui-logo_blue.png');
-    //}
-//}    
-
-function changeTheme(){
-    if (selectElem.value==='dark') {
-        document.body.classList.add('dark');
-        logo.src='images/byui-logo_white.png';
-    }
-    else {
-        document.body.classList.remove('dark');
-        logo.src='images/byui-logo_blue.png';
-    }
-}
                     
 
 
