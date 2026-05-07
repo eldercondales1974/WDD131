b56cfb6a6a57ee4b67ebd9bfee37382a2ddea614
eldercondales1974 <eldercondales@proton.me>
2026-05-06 22:36:26 -0400

    Last commit today

--- mission/mission.js
+++ mission/mission.js
@@ -17,3 +17,39 @@ function changeTheme() {
 themeSelector.addEventListener('change', changeTheme);
 
 
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('#logo');
+let body = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+//function changeTheme() {
+    //let current = selectElem.value;
+    //console.log(current);
+    //if (current === 'dark') {
+        // code for changes to colors and logo
+        //document.body.style.backgroundColor = 'black';
+        //document.body.style.color = '#ffffff';
+        //logo.setAttribute('src', 'images/byui-logo_white.png');
+
+    //} else {
+        // code for changes to colors and logo
+        //document.body.style.backgroundColor = '#ffffff';
+        //document.body.style.color = '#000000';
+        //logo.setAttribute('src', 'images/byui-logo_blue.png');
+    //}
+//}    
+
+function changeTheme(){
+    if (selectElem.value==='dark') {
+        document.body.classList.add('dark');
+        logo.src='images/byui-logo_white.png';
+    }
+    else {
+        document.body.classList.remove('dark');
+        logo.src='images/byui-logo_blue.png';
+    }
+}
+                    
+
+
